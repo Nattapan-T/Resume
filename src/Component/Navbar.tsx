71ddd261dfd65735e55fb96e3a909a0d98505482
nattapan <nattapan.code@gmail.com>
2023-09-05 14:43:56 +0700

    Toggle navbar and fix mobile responsive

--- src/Component/Navbar.tsx
+++ src/Component/Navbar.tsx
@@ -6,51 +6,32 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
-  const [NavItemActive, setNavItemActive] = useState<string | null>(null);
+  const [navItemActive, setNavItemActive] = useState<boolean>(false);
 
-  const activateItem = (item: string) => {
-    if (NavItemActive) {
-      const prevActiveItem = document.getElementById(NavItemActive);
-      if (prevActiveItem) {
-        prevActiveItem.classList.remove("active");
-      }
-    }
-    setNavItemActive(item);
-    const newActiveItem = document.getElementById(item);
-    if (newActiveItem) {
-      newActiveItem.classList.add("active");
-    }
+  const toggleNav = () => {
+    setNavItemActive(!navItemActive);
+  };
+
+  const closeNav = () => {
+    setNavItemActive(false);
   };
 
   return (
     <nav>
       <button
         id="nav-toggle"
-        className="toggle-button"
-        onClick={() => {
-          const navUl = document.querySelector("nav ul");
-          if (navUl) {
-            navUl.classList.toggle("open");
-          }
-        }}
+        className={`toggle-button ${navItemActive ? "open" : ""}`}
+        onClick={toggleNav}
       >
         <FontAwesomeIcon icon={faBars} />
       </button>
-      <ul>
-        <NavItems item="Home" tolink="/" activenav={activateItem} />
-        <NavItems item="About" tolink="/about" activenav={activateItem} />
-        <NavItems
-          item="Education"
-          tolink="/education"
-          activenav={activateItem}
-        />
-        <NavItems
-          item="Experience"
-          tolink="/experience"
-          activenav={activateItem}
-        />
-        <NavItems item="Skill" tolink="/skill" activenav={activateItem} />
-        <NavItems item="Contact" tolink="/contact" activenav={activateItem} />
+      <ul className={`nav-list ${navItemActive ? "open" : ""}`}>
+        <NavItems item="Home" tolink="/" activenav={closeNav} />
+        <NavItems item="About" tolink="/about" activenav={closeNav} />
+        <NavItems item="Education" tolink="/education" activenav={closeNav} />
+        <NavItems item="Experience" tolink="/experience" activenav={closeNav} />
+        <NavItems item="Skill" tolink="/skill" activenav={closeNav} />
+        <NavItems item="Contact" tolink="/contact" activenav={closeNav} />
       </ul>
     </nav>
   );
